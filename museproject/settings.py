4394aa6e33592c5c93ab219e062a0e042762dcb3
Sodbridge Academy <118179277+sodbridgeacademy@users.noreply.github.com>
2023-02-22 00:04:19 +0100

    Update settings.py

--- museproject/settings.py
+++ museproject/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-f$3$=i63%@ddwkcnl8on5=vlm&wyeqo&8@!%6e0f3l(e1!j^fq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'dietmuse-env.eba-6nksmzmi.us-west-2.elasticbeanstalk.com', 'localhost', 'thedietmuse.click', 'www.thedietmuse.click']
+ALLOWED_HOSTS = ['*', 'web-production-2e12.up.railway.app', 'localhost', 'thedietmuse.click', 'www.thedietmuse.click']
 
 #gmail smtp
 # EMAIL_HOST = 'smtp.gmail.com'
