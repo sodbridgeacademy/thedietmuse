ebd7ab1349b76722cb71f50b24c108950df84978
Sodbridge Academy <118179277+sodbridgeacademy@users.noreply.github.com>
2023-02-22 08:02:21 +0100

    Update settings.py

--- museproject/settings.py
+++ museproject/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 #STATIC_ROOT = BASE_DIR / 'static'
 #STATIC_ROOT = os.path.join(BASE_DIR, "static")
@@ -137,8 +139,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
